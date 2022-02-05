7510da178ba23eb21c8cd81b42c0907975d6e1c5
Shehroz Ali <sshehroza13@gmail.com>
2022-02-05 16:42:01 +0500

    Moved Index sections

--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ import "assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
 import "App.css";
 
+// Pages
 import Index from "views/Index.js";
 import About from "views/About.js";
 import Success from "views/Success.js";
+
+// Sections
+import Technology from "views/sections/Technology.js";
+import Consulting from "views/sections/Consulting.js";
+import Services from "views/sections/Services.js";
+import Hardware from "views/sections/Hardware.js";
+
 
 ReactDOM.render(
   <BrowserRouter>
--- src/views/Index.js
+++ src/views/Index.js
@@ -1,17 +1,18 @@
 import React from "react";
 
+// Components
 import Navbar from "components/Navbar/Navbar.js";
 import Hero from "components/Header/Hero.js";
-import Intro from "views/sections/Intro.js";
+import Footer from "components/Footer/Footer.js";
 
+// Index Sections
+import IntroSection from "views/IndexViews/IntroSection";
 import Heading from "views/IndexViews/Heading.js";
 import TechnologySection from "views/IndexViews/TechnologySection.js";
 import ConsultingSection from "views/IndexViews/ConsultingSection.js";
 import ServicesSection from "views/IndexViews/ServicesSection.js";
 import HardwareSection from "views/IndexViews/HardwareSection.js";
-
-import Booking from "views/sections/Booking";
-import Footer from "components/Footer/Footer.js";
+import BookingSection from "views/IndexViews/BookingSection.js";
 
 export default function Index() {
   React.useEffect(() => {
@@ -33,17 +34,13 @@ export default function Index() {
           link="/#Booking"
         />
         <div className="main">
-          <Intro />
-
+          <IntroSection />
           <Heading />
           <TechnologySection />
           <ConsultingSection />
           <ServicesSection />
           <HardwareSection />
-
-
-
-          <Booking />
+          <BookingSection />
         </div>
         <Footer />
       </div>
